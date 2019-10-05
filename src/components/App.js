c435d9db774c5c1280feb30c5443a2b6d6322087
VictorParodi <victor.parodi.montero@gmail.com>
2019-10-05 17:53:32 -0500

    app1 commit 2: creating the UserCreate, Field and Button component

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import UserCreate from './UserCreate.js';
 
 class App extends React.Component {
     state = {language: 'english'};
@@ -13,7 +14,7 @@ class App extends React.Component {
                 <h1>Select a language: </h1>
                 <i className="flag us" onClick={() => this.onChangeLanguage('english')}></i>
                 <i className="flag nl" onClick={() => this.onChangeLanguage('dutch')}></i>
-                {this.state.language}
+                <UserCreate />
             </div>
         );
     }
