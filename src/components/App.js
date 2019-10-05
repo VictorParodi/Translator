f7f602ff64de5603f722570aab9333cd9564db93
VictorParodi <victor.parodi.montero@gmail.com>
2019-10-05 17:33:06 -0500

    app1 commit 1: changin the language in the state

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
 
 class App extends React.Component {
+    state = {language: 'english'};
+
+    onChangeLanguage = (lang) => {
+        this.setState({language: lang});
+    }
+
     render() {
         return (
             <div className="ui container">
-                <h1>App Component</h1>
+                <h1>Select a language: </h1>
+                <i className="flag us" onClick={() => this.onChangeLanguage('english')}></i>
+                <i className="flag nl" onClick={() => this.onChangeLanguage('dutch')}></i>
+                {this.state.language}
             </div>
         );
     }
