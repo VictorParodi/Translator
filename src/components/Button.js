762bfb24ce43a5fbcc90c615feb9155570c7ce8d
VictorParodi <victor.parodi.montero@gmail.com>
2019-10-05 21:12:07 -0500

    app1 commit 3: creating a context object

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import LanguageContext from './../contexts/LanguageContext.js';
 
 class Button extends React.Component {
+    static contextType = LanguageContext;
+
     render() {
+        console.log('BUTTON CONTEXT --->', this.context);
         return (
             <button className="ui primary button">Submit</button>
         );
